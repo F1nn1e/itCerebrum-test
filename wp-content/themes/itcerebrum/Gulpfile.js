99b3a83720346dbe0d440eb4d7cb273c914ced44
F1nn1e <61849859+F1nn1e@users.noreply.github.com>
2020-03-28 13:11:29 +0200

    test gulp

--- wp-content/themes/itcerebrum/Gulpfile.js
+++ wp-content/themes/itcerebrum/Gulpfile.js
@@ -1,6 +1,17 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
 
-exports.default = defaultTask
+// Загрузка плагинов
+var gulp = require('gulp');
+var concatenate = require('gulp-concat-css');
+var minify = require('gulp-cssmin');
+// Конфигурация задач
+gulp.task('default', function(done) {
+    gulp.src('src/*.css')
+    // Конкатенация css файлов в директории src
+        .pipe(concatenate('styles.min.css'))
+        // Сжатие файлов CSS
+        .pipe(minify())
+        // Сохранение сжатого файла с директории css
+        .pipe(gulp.dest('css/'));
+    done()
+});
+// Использование команды `gulp` для запуска задач
